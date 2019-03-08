f361132b4c6573b971e788fa7b0ea9e4236730e2
Becky <rebeccajoynielsen@gmail.come>
2019-03-08 15:54:10 +0000

    first extension done

--- room.rb
+++ room.rb
@@ -3,10 +3,11 @@ class Room
   attr_reader :room_name, :playlist, :guests
 
 
-  def initialize(room_name)
+  def initialize(room_name, capacity)
     @room_name = room_name
     @playlist = []
     @guests = []
+    @capacity = capacity
   end
 
   def add_song_to_playlist(song)
@@ -14,12 +15,22 @@ class Room
   end
 
   def add_guest_to_room(guest)
-    @guests.push(guest)
+    unless room_is_full?() == true
+      @guests.push(guest)
+    end
   end
 
   def remove_guest_from_room(guest_to_boot)
     @guests.delete_if { |guest| guest == guest_to_boot }
-
   end
+
+  def room_is_full?
+   if @guests.length >= @capacity
+     return true
+   else
+     return false
+   end
+  end
+
 
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -7,14 +7,17 @@ require_relative("../guest")
 class TestRoom < MiniTest::Test
 
   def setup
-    @room_01 = Room.new("Room 1")
+    @room_01 = Room.new("Room 1", 4)
 
     @song_01 = Song.new("Bohemian Rhapsody", "Queen")
     @song_02 = Song.new("Hey Jude", "The Beatles")
     @song_03 = Song.new("Thank u, next", "Ariana Grande")
 
-    @guest_01 = Guest.new("Freda")
+    @guest_01 = Guest.new("Anni-Frid")
     @guest_02 = Guest.new("Benny")
+    @guest_03 = Guest.new("Agnetha")
+    @guest_04 = Guest.new("Bjorn")
+    @guest_05 = Guest.new("Freda")
   end
 
   def test_does_room_exist
@@ -42,5 +45,35 @@ class TestRoom < MiniTest::Test
     actual = @room_01.guests
     assert_equal([@guest_02], actual)
   end
-  
+
+  def test_check_capacity__room_full
+    @room_01.add_guest_to_room(@guest_01)
+    @room_01.add_guest_to_room(@guest_02)
+    @room_01.add_guest_to_room(@guest_03)
+    @room_01.add_guest_to_room(@guest_04)
+    actual = @room_01.room_is_full?
+    assert_equal(true, actual)
+  end
+
+  def test_check_capacity__room_not_full
+    @room_01.add_guest_to_room(@guest_01)
+    @room_01.add_guest_to_room(@guest_02)
+    @room_01.add_guest_to_room(@guest_03)
+    actual = @room_01.room_is_full?
+    assert_equal(false, actual)
+  end
+
+  def
+    test_add_guest_to_full_room
+    @room_01.add_guest_to_room(@guest_01)
+    @room_01.add_guest_to_room(@guest_02)
+    @room_01.add_guest_to_room(@guest_03)
+    @room_01.add_guest_to_room(@guest_04)
+    assert_equal(4, @room_01.guests.length)
+
+    @room_01.add_guest_to_room(@guest_05)
+    assert_equal([@guest_01, @guest_02, @guest_03, @guest_04], @room_01.guests)
+  end
+
+
 end
